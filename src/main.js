49429bae48614b35010b1621e67ed701e67f2015
Beeb <opeyemihabeebullah279@gmail.com>
2025-05-10 01:53:08 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -11,11 +11,11 @@ document.addEventListener('DOMContentLoaded', () => {
   async function initializeWalletConnect() {
     console.log('Starting WalletConnect initialization');
     try {
-      const projectId = '19f08313224ac846097e6a722ab078fc';
+      const projectId = '02e1bc316278f55430a587c11db76048';
       const metadata = {
         name: 'Overlayz',
         description: 'NFT Overlay Tool for Hedera',
-        url: 'https://hederanftoverlayz.vercel.app',
+        url: 'https://hedera-nft-overlay-new.vercel.app/',
         icons: ['/assets/icon/Overlayz_App_Icon.png'],
       };
 
